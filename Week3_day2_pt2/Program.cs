c0f1422c2039e58eb93836c6996286b87640674e
Nathan Pawloski <natepawloski@gmail.com>
2017-10-10 13:04:48 -0400

    initial commit

---
+++ Week3_day2_pt2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day2_pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
